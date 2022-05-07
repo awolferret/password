2ad678c2325d8b2bdb0e48a18742178ea4049100
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-07 18:40:28 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "0451";
+            int trys = 3;
+            string tryPassword;
+            Console.WriteLine("Введите пароль");
+
+            for (int i = 0; trys > 0; trys--)
+            {
+                Console.WriteLine($"У вас осталось {trys} попыток");
+                tryPassword = Console.ReadLine();
+
+                if (tryPassword == password)
+                {
+                    Console.WriteLine("Секретное сообщение");
+                    break;
+                }
+            }
+        }
+    }
+}
